fef3e4fcefac6971e357ebc538ece15c95a6e24d
FC-Sebastian <sebastian.briese@fatchip.de>
2023-03-30 13:22:06 +0200

    initial comit

---
+++ metadata.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Metadata version
+ */
+$sMetadataVersion = '2.1';
+/**
+ * Module information
+ */
+$aModule = [
+    'id'           => 'seb_banner',
+    'title'        => 'Banner placeholder title',
+    'description'  => 'placeholder description or this module',
+    'version'      => '1.0',
+    'author'       => 'FC-Sebastian',
+    'controllers'  => [],
+    'templates'    => [],
+    'blocks'       => [
+        [
+            'template' => 'layout/page.tpl',
+            'block'    => 'content_main',
+            'file'     => 'views/blocks/banner.tpl'
+        ]
+    ]
+];
